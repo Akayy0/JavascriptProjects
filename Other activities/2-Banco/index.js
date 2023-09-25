ec574037e7eb70c9f616459cd171d126ead78825
João Vitor <101755765+Akayy0@users.noreply.github.com>
2023-09-25 16:55:20 -0300

    new functionalities activity 2

--- Other activities/2-Banco/index.js
+++ Other activities/2-Banco/index.js
@@ -1,31 +1,52 @@
 class account {
-    constructor(titular, saldo){
+    constructor(titular, saldo, limite = 1000, transacoes){
         this.titular = titular;
         this.saldo = saldo || 0;
+
+        this.limite = limite;
+        this.transacoes = [];
     }
 
     deposit(value){
 
         this.saldo += value
         console.log(`Voce depositou ${value}, saldo atual e de: ${this.saldo}`);
+        this.regist("Deposito", value);
     }
 
     withdraw(value){
-        if(value > 500){
+        if(value > this.limite){
+            console.log("Valor de saque excedido")
+        }else{
+            if(value > 500){
 
-            let taxa = value * 0.05;
-            this.saldo -= taxa
-            value -= taxa;
-            console.log(`Foi cobrado uma taxa de 5% no seu saque no valor de ${taxa}`);
+                let taxa = value * 0.05;
+                this.saldo -= taxa
+                value -= taxa;
+                console.log(`Foi cobrado uma taxa de 5% no seu saque no valor de ${taxa}`);
+            }
+    
+            if (value > this.saldo){
+                console.log('Saldo insuficiente');
+                return;
+            }
+    
+            this.saldo -= value;
+            console.log(`Saque de ${value} realizado. Saldo atual de ${this.saldo}`);
+    
+            this.regist("Saque", value)
         }
+        
+    }
 
-        if (value > this.saldo){
-            console.log('Saldo insuficiente');
-            return;
-        }
+    regist(descrição, value){
+        const transacao ={
+            descrição: descrição,
+            valor: value
+        };
 
-        this.saldo -= value;
-        console.log(`Saque de ${value} realizado. Saldo atual de ${this.saldo}`);
+        this.transacoes.push(transacao);
+
     }
 
 }
@@ -38,4 +59,6 @@ Account1.deposit(1000)
 console.log(Account1);
 
 Account1.withdraw(800)
+
+console.log(Account1.transacoes)
 
