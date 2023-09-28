7396943b5ade18b7eccba7e5f728c1c7745cb01d
João Vitor <101755765+Akayy0@users.noreply.github.com>
2023-09-28 16:01:33 -0300

    GET and POST methods

--- Other activities/6-Produto/index.js
+++ Other activities/6-Produto/index.js
@@ -4,12 +4,54 @@ class Produto{
         this.preço = preço;
         this.quantidade = quantidade;
     }
+
+    static listarProdutos(){
+        return fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response =>{
+                if(!response.ok){
+                    throw new Error('Erro ao listar produtos')
+                }
+                return response.json();
+            })
+            .then(data =>{
+                return data.map(item => new Produto(item.title, 10.0, 10));
+            });
+    }
+
+    static adicionarProduto(produto) {
+
+        return fetch('https://jsonplaceholder.typicode.com/todos', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            title: produto.nome,
+            userId: 1,
+            completed: false,
+          }),
+        })
+        .then(response =>{
+            if(!response.ok){
+                throw new Error('Erro ao adicionar produto')
+            }
+            return response.json();
+        })
+        .then(data =>{
+            return new Produto(data.title, 10.0, 100);
+        });
+    }
+
+
 }
 
 
-const Camiseta = new Produto('Camiseta Branca', 50, 5)
+// Produto.listarProdutos().then(produtos =>{
+//     console.log('Listar produtos: ', produtos);
+// });
 
+Produto.adicionarProduto(new Produto('Novo Produto', 15.0, 50)).then(novoProduto =>{
+    console.log('Produto adicionado: ', novoProduto)
+})
 
-
-console.log(Camiseta)
 
