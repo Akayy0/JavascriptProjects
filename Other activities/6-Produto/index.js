16616eef7726c5483b8ffc889746d1a14b0daf31
João Vitor <101755765+Akayy0@users.noreply.github.com>
2023-10-03 00:47:38 -0300

    async/await implementation

--- Other activities/6-Produto/index.js
+++ Other activities/6-Produto/index.js
@@ -1,48 +1,55 @@
-class Produto{
-    constructor(nome, preço, quantidade){
+class Produto {
+    constructor(nome, preço, quantidade) {
         this.nome = nome;
         this.preço = preço;
         this.quantidade = quantidade;
     }
 
-    static listarProdutos(){
-        return fetch('https://jsonplaceholder.typicode.com/todos')
-            .then(response =>{
-                if(!response.ok){
-                    throw new Error('Erro ao listar produtos')
-                }
-                return response.json();
-            })
-            .then(data =>{
-                return data.map(item => new Produto(item.title, 10.0, 10));
-            });
+    static async listarProdutos() {
+
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+
+            if (!response.ok) {
+                throw new Error('Erro ao listar produtos')
+            }
+
+            const data = await response.json();
+
+            return data.map(item => new Produto(item.title, 10.0, 10));
+        }catch(error){
+            console.error(error)
+        }
+
+        
+
     }
 
     static adicionarProduto(produto) {
 
         return fetch('https://jsonplaceholder.typicode.com/todos', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            title: produto.nome,
-            userId: 1,
-            completed: false,
-          }),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: produto.nome,
+                userId: 1,
+                completed: false,
+            }),
         })
-        .then(response =>{
-            if(!response.ok){
-                throw new Error('Erro ao adicionar produto')
-            }
-            return response.json();
-        })
-        .then(data =>{
-            return new Produto(data.title, 10.0, 100);
-        });
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao adicionar produto')
+                }
+                return response.json();
+            })
+            .then(data => {
+                return new Produto(data.title, 10.0, 100);
+            });
     }
 
-    static atualizarProduto(id, novoNome){
+    static atualizarProduto(id, novoNome) {
         return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
             method: 'PUT',
             headers: {
@@ -54,23 +61,23 @@ class Produto{
         })
 
             .then(response => {
-                if(!response.ok){
+                if (!response.ok) {
                     throw new Error('Erro ao atualizar produto');
                 }
                 return response.json();
             })
-            .then(data =>{
+            .then(data => {
                 return new Produto(data.title, 10.0, 100)
             })
     }
 
-    static excluirProduto(id){
+    static excluirProduto(id) {
         return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
             method: 'DELETE'
         })
 
-            .then(response =>{
-                if(!response.ok){
+            .then(response => {
+                if (!response.ok) {
                     throw new Error('Erro ao excluir produto');
                 }
             });
@@ -82,20 +89,20 @@ class Produto{
 }
 
 
-Produto.listarProdutos().then(produtos =>{
+Produto.listarProdutos().then(produtos => {
     console.log('Listar produtos: ', produtos);
 });
 
-Produto.adicionarProduto(new Produto('Novo Produto', 15.0, 50)).then(novoProduto =>{
+Produto.adicionarProduto(new Produto('Novo Produto', 15.0, 50)).then(novoProduto => {
     console.log('Produto adicionado: ', novoProduto);
 });
 
-Produto.atualizarProduto(1, 'Produto Atualizado').then(produtoAtualizado =>{
+Produto.atualizarProduto(1, 'Produto Atualizado').then(produtoAtualizado => {
     console.log('Produto atualizado: ', produtoAtualizado);
 });
 
 Produto.excluirProduto(1).then(() => {
     console.log('Produto excluído com sucesso');
-  });
+});
 
 
