199079c26a6fe8d418d206cb146ad8c5bc1c1549
João Vitor <101755765+Akayy0@users.noreply.github.com>
2023-10-03 16:01:08 -0300

    activity 13 part 2

--- Other activities/13-Posts/index.js
+++ Other activities/13-Posts/index.js
@@ -1,3 +1,15 @@
+class Postagens {
+    constructor(title, body){
+        this.title = title;
+        this.body = body;
+    }
+}
+
+const Post1 = new Postagens('Titulo teste', 'Conteudo teste');
+
+const Post2 = new Postagens('Titulo atualizado', 'Conteudo atualizado');
+
+
 const showPosts = async () =>{
     try{
 
@@ -19,4 +31,93 @@ const showPosts = async () =>{
     }
 }
 
+
+const postPosts = async (post) =>{
+
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts',{
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json',
+            },
+            body: JSON.stringify({
+                title: post.title,
+                body: post.body,
+                userId: 1,
+            })
+        })
+
+        if (!response.ok){
+            throw new Error('Erro no post')
+        }
+
+        const postagem = response.json();
+
+        console.log('Nova postagem criada com sucesso!!', postagem)
+
+        
+
+        
+
+    }catch(error){
+        console.error(error)
+    }
+}
+
+
+
+const updatePosts = async (id, updatePost) =>{
+    try{
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{
+            method: 'PUT',
+            headers:{
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatePost)
+        })
+
+        if(!response.ok){
+            throw new Error('Erro ao atualizar o post')
+        }
+
+        const data = response.json();
+
+        console.log('Atualização do post completa: ', data)
+
+    }catch(error){
+        console.error(error)
+    }
+}
+
+const deletePosts = async (id) =>{
+    try{
+
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{
+            method: 'DELETE'
+        })
+
+        if(!response.ok){
+            throw new Error('Erro ao apagar postagem')
+        }
+
+        if(response.status === 200 || response.status === 204){
+            console.log('O Post foi deletado com sucesso')
+        }
+
+
+    }catch(error){
+        console.error(error)
+    }
+}
+
+
 showPosts()
+
+postPosts(Post1)
+
+updatePosts(1, Post2)
+
+deletePosts(1)
+
+
+
